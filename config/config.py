7273af207c5823090db2c18587854a33857e23ab
commando <ahmedkhalilbejaoui@gmail.com>
2019-08-18 15:50:52 +0200

    [CONFIG] add config.ini reader and set auth and project config

---
+++ config/config.py
@@ -0,0 +1,35 @@
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser
+
+class Config():
+    def __init__(self):
+        self.config = ConfigParser()
+        self.config.read('config.ini')
+
+        if (not self.config.sections()):
+            self.createSections()
+
+
+
+    def createSections(self):
+        working_dir = raw_input("Project Name: ") 
+        username = raw_input("Bitbucket Username: ")
+        password = raw_input("Bitbucket Password: ")
+        projectKey = raw_input("Project Key: ")
+        team = raw_input("Team: ")
+
+        config = ConfigParser()
+        config.read('config.ini')
+        config.add_section('AUTH')
+        config.set('AUTH', 'username', username)
+        config.set('AUTH', 'password', password)
+
+        config.add_section('PROJECT')
+        config.set('PROJECT', 'projectName', working_dir)
+        config.set('PROJECT', 'projectKey', projectKey)
+        config.set('PROJECT', 'team', team)
+
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
